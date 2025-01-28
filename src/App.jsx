d8108d1307c9dc9392d62c462fd1b9c192e73691
Joshua Duke <joshuadukedev@gmail.com>
2025-01-27 19:41:16 -0500

    Complete main hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import { useState } from "react";
 import "./App.css";
+import Search from "./components/Home/Search";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <main>
       <div className="pattern" />
+
       <div className="wrapper">
         <header>
+          <img src="./hero.png" alt="Hero Banner" />
           <h1>
             Find <span className="text-gradient">Movies</span> you&apos;ll Enjoy
             Without the Hassle
           </h1>
         </header>
-        <p>Search</p>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </div>
     </main>
   );
